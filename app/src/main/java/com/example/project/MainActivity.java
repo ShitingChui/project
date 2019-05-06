4118bb42e86b2fbdb27530ee76740f886338ba45
ShitingChui <tinapq212pq212@gmail.com>
2019-05-07 01:39:23 +0800

    0507_2
    part3

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.project;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
 
     @Override
@@ -19,11 +22,57 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new SportFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_sport);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_sport:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SportFragment()).commit();
+                break;
+            case R.id.nav_sport_record:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SportRecorderFragment()).commit();
+                break;
+            case R.id.nav_suggestion:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SportSuggestionFragment()).commit();
+                break;
+            case R.id.nav_diet_record:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new DietFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_reporter:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ReporterFragment()).commit();
+                break;
+            case R.id.nav_log_out:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new LogoutFragment()).commit();
+                break;
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
